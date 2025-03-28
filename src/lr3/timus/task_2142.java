dbb7ba699307b2463d62649cfc9ef7ac8e8380cc
bsr <mursik1602@gmail.com>
2025-03-28 17:27:38 +0500

    Completed task_2142

---
+++ src/lr3/timus/task_2142.java
@@ -0,0 +1,27 @@
+package lr3.timus;
+
+import java.util.Scanner;
+
+public class task_2142 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Чтение входных данных
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        long X = scanner.nextLong();
+        long Y = scanner.nextLong();
+        long Z = scanner.nextLong();
+
+        // Проверка условий
+        if (A + B + C >= X + Y + Z &&
+                A + C >= X &&
+                B + C >= Y) {
+            System.out.println("It is a kind of magic");
+        } else {
+            System.out.println("There are no miracles in life");
+        }
+    }
+}
